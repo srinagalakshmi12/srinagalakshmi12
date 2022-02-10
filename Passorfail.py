25ce8610dc29081bef983f8df600cd90654fb70d
srinagalakshmi12 <99389837+srinagalakshmi12@users.noreply.github.com>
2022-02-10 19:13:24 +0530

    Create Passorfail.py

---
+++ Passorfail.py
@@ -0,0 +1,23 @@
+- 👋 Hi, I’m @srinagalakshmi12
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+srinagalakshmi12/srinagalakshmi12 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+Sno=int(input('enter sno'))
+Sname=int(input('enter sname'))
+Sgroup=input('enter group'))
+S1=int(input('telugu marks'))
+S2=int(input('english marks'))
+S3=int(input('ee marks'))
+S4=int(input('maths marks'))
+S5=int(input('chemistry marks'))
+S6=int(input('computer marks'))
+if S1>35 and S2>35 and S3>35 and S4>35 and S5>35 and S6>35:
+      Print('pass')
+else:
+      Print('fail')
