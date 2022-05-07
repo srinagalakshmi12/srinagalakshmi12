d2c6b7310f0942146ed615de2a05077ffb56d17f
srinagalakshmi12 <99389837+srinagalakshmi12@users.noreply.github.com>
2022-05-07 19:56:33 +0530

    Create yes even and no odd.m

---
+++ yes even and no odd.m
@@ -0,0 +1,12 @@
+n=int(input())
+l=[]
+d={}
+for i in range(n):
+    x=int(input())
+    l.append(x)
+    j=1
+for i in l:
+    if i%2==0:
+        print('yes',end=' ')
+    else:
+        print('no',end=' ')
